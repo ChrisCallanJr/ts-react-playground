f707006f5e285b008d25c271bb6c0de6795599a3
Chris Callan <>
2018-08-05 13:34:37 -0400

    👩‍🎤 first successful emotion test

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
+import { css } from 'emotion'
+
+const navStyle = css`
+  color: #FFF;
+  background-color: #424242;
+  height: 150px;
+  width: 150px;
+`
 
 class MainNav extends React.Component<{}, {}>{
   render() {
     return(
-      <div>
+      <div className={ navStyle }>
         <h1>Test</h1>
       </div>
     )
